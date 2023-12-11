a0d4792702d9d9af8df9fbc171eeea22d60d1e4d
prajjwal ajit <122018880+Prajjwalajit15@users.noreply.github.com>
2023-12-11 17:42:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  makeSound() {
+    console.log("Generic animal sound");
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log("Woof");
+  }
+}
+
+class Cat extends Animal {
+  purr() {
+    console.log("Purr");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
+
